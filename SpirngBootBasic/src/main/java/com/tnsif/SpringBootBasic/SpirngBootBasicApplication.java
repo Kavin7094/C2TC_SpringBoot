ffdb7151605374b34b6d06a9412629961daf1053
Kavin7094 <kavin7094581918@gmail.com>
2025-09-08 21:19:15 +0530

    Spring boot basic

---
+++ SpirngBootBasic/src/main/java/com/tnsif/SpringBootBasic/SpirngBootBasicApplication.java
@@ -0,0 +1,15 @@
+package com.tnsif.SpringBootBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngBootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngBootBasicApplication.class, args);
+		
+		System.out.println("Hello World !!!");
+	}
+
+}
---
+++ StudenServices/src/test/java/com/tnsif/Student/StudenServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.Student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
